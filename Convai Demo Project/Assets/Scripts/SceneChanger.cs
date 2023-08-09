0525b5343b5211630014e6f3841e7424de88a1fd
kedzkiest <kaede12kazu22@gmail.com>
2023-08-09 22:40:15 +0900

    オープニングアニメーションを追加

--- Convai Demo Project/Assets/Scripts/SceneChanger.cs
+++ Convai Demo Project/Assets/Scripts/SceneChanger.cs
@@ -3,13 +3,23 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
+    [SerializeField] private string mainSceneName;
+
     // Update is called once per frame
     private void Update()
     {
         // Scene reload function in case we need to reload the system due to unexpeted bugs, etc..
         if(Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.R))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            SceneManager.LoadScene("Opening");
         }
+    }
+
+    /// <summary>
+    /// Scene transition Opening -> Main
+    /// </summary>
+    public void GoToMain()
+    {
+        SceneManager.LoadScene(mainSceneName);
     }
 }
